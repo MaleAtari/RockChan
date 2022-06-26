54a6c535b128fe61444d125ec61311fea3eaeac3
MaleAtari <monhal95@o2.pl>
2022-06-26 20:13:41 +0200

    dalej blog

--- meine/blog/views.py
+++ meine/blog/views.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template
+from meine.models import Board
 
 blog_blueprint = Blueprint('blog', __name__, template_folder='templates')
 
 @blog_blueprint.route('/')
 def home():
-    return render_template('blog/home.html')
+    boards = Board.query.all()
+    return render_template('blog/home.html', boards=boards)
 
 
 @blog_blueprint.route('/board/<name>')
