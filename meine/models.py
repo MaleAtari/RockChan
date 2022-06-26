3c5ec2b832d66e5706eb67ae148e0cdbf20a6ec6
MaleAtari <monhal95@o2.pl>
2022-06-26 19:05:46 +0200

    baza faza 2

---
+++ meine/models.py
@@ -0,0 +1,22 @@
+from meine import db
+from datetime import datetime
+
+
+class Board(db.Model):
+    __tablename__ = 'board'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256))
+    date_add = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    info = db.Column(db.Text)
+    posts = db.relationship('Posts', backref='boards')
+
+class Posts(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date_add = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    auth = db.Column(db.String(128))
+    content = db.Column(db.Text)
+    board_id = db.Column(db.ForeignKey(Board.id))
+
