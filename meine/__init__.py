d0e744d27d0585af94707591c039c086ab2363d9
MaleAtari <monhal95@o2.pl>
2022-06-26 18:50:20 +0200

    baza faza 1

--- meine/__init__.py
+++ meine/__init__.py
@@ -1,3 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+# # # CONFIG APP
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///baza.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# # # BAZA
+db = SQLAlchemy(app)
+Migrate(app, db)
+
