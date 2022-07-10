652395f95cdca9c4521879351e9d4fd593e7690d
MaleAtari <monhal95@o2.pl>
2022-07-10 21:08:08 +0200

    panel admin cz 1

--- meine/users/forms.py
+++ meine/users/forms.py
@@ -1,6 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, RadioField
 from wtforms.validators import data_required
+from meine.models import Board
+
+
+def board_list():
+    default = (0, 'Nic nie usuwaj')
+    choice = [default]
+    for board in Board.query.all():
+        choice.append((board.id, board.name))
+
+    return choice
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +18,17 @@ class LoginForm(FlaskForm):
     name = StringField('Podaj Login', validators=[data_required()])
     password = PasswordField('Podaj Hasło', validators=[data_required()])
     submit = SubmitField('Potwierdz')
+
+class AdminControlForm(FlaskForm):
+    submit = SubmitField('Zapisz zmiany')
+
+class BoardForm(FlaskForm):
+    name = StringField('Nazwa Tablicy', validators=[data_required()])
+    info = TextAreaField('Opis Tablicy')
+    submit = SubmitField('Dodaj Tablicę')
+
+class DelBoardForm(FlaskForm):
+    menu = RadioField('Tablica do usunięcia')
+    submit = SubmitField('Usuń !!!')
+
+
--- meine/users/views.py
+++ meine/users/views.py
@@ -1,9 +1,17 @@
-from flask import Blueprint, render_template, flash, redirect, url_for
-from meine.users.forms import LoginForm
-from meine.models import Users
-from flask_login import login_user, login_required, logout_user
+from flask import Blueprint, render_template, flash, redirect, url_for, request
+from meine.users.forms import LoginForm, BoardForm, DelBoardForm
+from meine.models import Users, Board, db
+from flask_login import login_user, login_required, logout_user, current_user
 
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
+
+def board_list():
+    default = (0, 'Nic nie usuwaj')
+    choice = [default]
+    for board in Board.query.all():
+        choice.append((board.id, board.name))
+
+    return choice
 
 @users_blueprint.route('/', methods=['POST', 'GET'])
 def home():
@@ -28,3 +36,41 @@ def home():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@users_blueprint.route('/admin')
+@login_required
+def admin():
+    if current_user.role > 1:
+        return redirect(url_for('home'))
+
+    return render_template('users/admin.html')
+
+@users_blueprint.route('/admin/board', methods=['POST', 'GET'])
+@login_required
+def add_board():
+    form = BoardForm()
+    if form.validate_on_submit():
+        new_board = Board(name=form.name.data, info=form.info.data)
+        db.session.add(new_board)
+        db.session.commit()
+
+        return redirect(url_for('blog.home'))
+
+    return render_template('users/add_board.html', form=form)
+
+@users_blueprint.route('/admin/delete', methods=['POST', 'GET'])
+@login_required
+def del_board():
+    boards = Board.query.all()
+
+    if request.method == 'POST':
+        deleted = Board.query.get(request.form['boardRadio'])
+        db.session.delete(deleted)
+        db.session.commit()
+
+        return redirect(url_for('users.del_board'))
+
+
+
+    return render_template('users/del_board.html', boards=boards)
+
